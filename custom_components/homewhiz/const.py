33520b52ad82638704f4e99f3b96f490495800df
Robert Wysocki <rowysock@gmail.com>
2022-12-01 20:11:44 +0100

    Rewrite setup, discovery and connect logic
    use bleak_retry_connector

--- custom_components/homewhiz/const.py
+++ custom_components/homewhiz/const.py
@@ -1,15 +1,16 @@
 # Base component constants
+from homeassistant.const import Platform
+
 NAME = "HomeWhiz"
 DOMAIN = "homewhiz"
 DOMAIN_DATA = f"{DOMAIN}_data"
+PLATFORMS = [Platform.SENSOR]
 VERSION = "0.0.0"
 
 ISSUE_URL = "https://github.com/rowysock/home-assistant-HomeWhiz/issues"
 
 # Icons
 ICON = "mdi:washing-machine"
-
-COORDINATORS = "coordinators"
 
 # Defaults
 DEFAULT_NAME = DOMAIN
