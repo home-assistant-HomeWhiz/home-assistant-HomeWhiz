265737a773d1ad481876f7fd7aea578ad2fbc5cd
maxx4096 <43723410+maxx4096@users.noreply.github.com>
2022-12-05 23:47:03 +0100

    Fix erroneous time delay when started (#35)
    * resolve conflicts
    
    * style for bytearray
    
    * Update test_parser.py
    
    Fixed copy paste error
    
    * hopefully this is the one
    
    Co-authored-by: maxx4096 <maxx4096@users.noreply.github.com>

--- custom_components/homewhiz/tests/test_parser.py
+++ custom_components/homewhiz/tests/test_parser.py
@@ -16,6 +16,16 @@ spinning = bytearray.fromhex(
     "00808080800211000a8080020000000000000000008080808180800000008081078000808000"
 )
 
+delay_defined = bytearray.fromhex(
+    "003853e0ab0100000000000000000000000000000000000000000300000000000000000a01280e"
+    "000000008002100210012c000000000000000000010000000100000000000001078000000000"
+)
+
+delay_started = bytearray.fromhex(
+    "003853e0ab0100000000000000000000000000000000000000000300000000000000003c01280e"
+    "00000000800210021081ac080000000000000000010000000100000000000001078000808000"
+)
+
 
 class TestParser(unittest.TestCase):
     def test_on(self):
@@ -50,6 +60,28 @@ class TestParser(unittest.TestCase):
         self.assertEqual(actual.remaining_minutes, 10)
         self.assertEqual(actual.delay_minutes, None)
         self.assertEqual(actual.device_sub_state, DeviceSubState.SPIN)
+
+    def test_delay_defined(self):
+        actual = parse_message(delay_defined)
+        self.assertEqual(actual.device_state, DeviceState.ON)
+        self.assertEqual(actual.temperature, 40)
+        self.assertEqual(actual.spin, 1400)
+        self.assertEqual(actual.rinse_hold, False)
+        self.assertEqual(actual.duration_minutes, 136)
+        self.assertEqual(actual.remaining_minutes, 136)
+        self.assertEqual(actual.delay_minutes, 104)
+        self.assertEqual(actual.device_sub_state, DeviceSubState.OFF)
+
+    def test_delay_started(self):
+        actual = parse_message(delay_started)
+        self.assertEqual(actual.device_state, DeviceState.TIME_DELAY_ACTIVE)
+        self.assertEqual(actual.temperature, 40)
+        self.assertEqual(actual.spin, 1400)
+        self.assertEqual(actual.rinse_hold, False)
+        self.assertEqual(actual.duration_minutes, 136)
+        self.assertEqual(actual.remaining_minutes, 136)
+        self.assertEqual(actual.delay_minutes, 104)
+        self.assertEqual(actual.device_sub_state, DeviceSubState.TIME_DELAY_ENABLED)
 
 
 if __name__ == "__main__":
