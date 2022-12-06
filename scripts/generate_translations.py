06372ca4e875c716bf16663162861e06ed70d7b6
Robert Wysocki <rowysock@gmail.com>
2022-12-06 23:41:08 +0100

    Generate translations from all the API languages

--- scripts/generate_translations.py
+++ scripts/generate_translations.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import os
 
 import aiohttp
 from dacite import from_dict
@@ -19,46 +20,81 @@ async def get_file(address: str):
             return json.loads(await response.text())
 
 
-localization_6_path = (
-    "https://s3-eu-west-1.amazonaws.com/procam-contents"
-    "/LOCALIZATIONS/LOCALIZATION_6/v592/LOCALIZATION_6.en-GB.json"
-)
-localization_30_path = (
-    "https://s3-eu-west-1.amazonaws.com/procam-contents"
-    "/LOCALIZATIONS/LOCALIZATION_30/v100/LOCALIZATION_30.en-GB.json"
-)
-config_216_path = (
+api_languages = {
+    "en": "en-GB",
+    "tr": "tr-TR",
+    "sv": "sv-SE",
+    "ro": "ro-RO",
+    "fi": "fi-FI",
+    "fr": "fr-FR",
+    "it": "it-IT",
+    "nb": "nb-NO",
+    "pl": "pl-PL",
+    "de": "de-DE",
+    "da": "da-DK",
+    "cs": "cs-CZ",
+    "es": "es-ES",
+    "pt": "pt-PT",
+}
+
+
+def localization_6(language: str):
+    return (
+        "https://s3-eu-west-1.amazonaws.com/procam-contents"
+        f"/LOCALIZATIONS/LOCALIZATION_6/v592/LOCALIZATION_6.{language}.json"
+    )
+
+
+def localization_30(language: str):
+    return (
+        "https://s3-eu-west-1.amazonaws.com/procam-contents"
+        f"/LOCALIZATIONS/LOCALIZATION_30/v100/LOCALIZATION_30.{language}.json"
+    )
+
+
+config_216 = (
     "https://s3-eu-west-1.amazonaws.com/procam-contents"
     "/CONFIGURATIONS/CONFIGURATION_216/v14/CONFIGURATION_216.en-GB.json"
 )
 
 
 async def generate():
-    localizations_6 = (await get_file(localization_6_path))["localizations"]
-    localizations_30 = (await get_file(localization_30_path))["localizations"]
-    localizations = localizations_6 | localizations_30
-    config = from_dict(ApplianceConfiguration, await get_file(config_216_path))
-
+    dirname = os.path.dirname(__file__)
+    translations_path = os.path.join(
+        dirname, f"../custom_components/homewhiz/translations/"
+    )
+    config = from_dict(ApplianceConfiguration, await get_file(config_216))
     descriptions = generate_descriptions_from_config(config)
 
-    def localize_key(key: str):
-        if key in localizations:
-            return localizations[key]
-        return key
+    for short_code in api_languages:
+        language = api_languages[short_code]
+        localizations_6 = (await get_file(localization_6(language)))["localizations"]
+        localizations_30 = (await get_file(localization_30(language)))["localizations"]
+        localizations = localizations_6 | localizations_30
 
-    def localize(description: EnumEntityDescription):
-        return {
-            option.strKey: localize_key(option.strKey) for option in description.options
+        def localize_key(key: str):
+            if key in localizations:
+                return localizations[key]
+            return key
+
+        def localize(description: EnumEntityDescription):
+            return {
+                option.strKey: localize_key(option.strKey)
+                for option in description.options
+            }
+
+        result = {
+            "state": {
+                description.device_class: localize(description)
+                for description in descriptions
+                if isinstance(description, EnumEntityDescription)
+            }
         }
 
-    result = {
-        "state": {
-            description.device_class: localize(description)
-            for description in descriptions
-            if isinstance(description, EnumEntityDescription)
-        }
-    }
-    print(json.dumps(result, indent=4))
+        file_path = os.path.join(translations_path, f"sensor.{short_code}.json")
+        with open(file_path, "w") as outfile:
+            json.dump(result, outfile, indent=2)
+        print(f"{file_path} Updated")
 
 
 if __name__ == "__main__":
