e97a6c9dd451cd0296aabcfb8b18e98d814f4f6a
TechHummel <15891478+TechHummel@users.noreply.github.com>
2024-04-16 23:29:50 +0200

    97 requirement not found awsiotsdk awscrt raspberrypi os 32 bit docker (#192)
    * Update requirements in manifest
    
    * Remove _lazy_install_awsiotsdk from init
    
    * Update github links in manifest
    
    * Add warning message aboutz compatibility to README

--- custom_components/homewhiz/__init__.py
+++ custom_components/homewhiz/__init__.py
@@ -11,8 +11,6 @@ from homeassistant.components.bluetooth import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import EVENT_HOMEASSISTANT_STOP
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.requirements import RequirementsNotFound
-from homeassistant.util.package import install_package, is_installed
 
 from .api import IdExchangeResponse
 from .bluetooth import HomewhizBluetoothUpdateCoordinator
@@ -74,16 +72,8 @@ async def setup_bluetooth(
     return True
 
 
-def _lazy_install_awsiotsdk() -> None:
-    custom_required_packages = ["awsiotsdk"]
-    for pkg in custom_required_packages:
-        if not is_installed(pkg) and not install_package(pkg):
-            raise RequirementsNotFound(DOMAIN, [pkg])
-
-
 async def setup_cloud(entry: ConfigEntry, hass: HomeAssistant) -> bool:
     _LOGGER.info("Setting up cloud connection")
-    _lazy_install_awsiotsdk()
 
     ids = from_dict(IdExchangeResponse, entry.data["ids"])
     cloud_config = from_dict(CloudConfig, entry.data["cloud_config"])
