6c009c3737366a49415c9ff2ae1853cd13b84cdb
RTM <rtm@insecure.codes>
2025-11-09 15:52:59 +0000

    Fix code formatting to pass ruff format check

--- custom_components/homewhiz/api.py
+++ custom_components/homewhiz/api.py
@@ -125,16 +125,18 @@ async def login(username: str, password: str) -> LoginResponse:
         contents = await response.json()
         if response.status != 200:
             _LOGGER.error(
-                "Login failed with HTTP %d: %s", response.status, json.dumps(
-                    contents, indent=4)
+                "Login failed with HTTP %d: %s",
+                response.status,
+                json.dumps(contents, indent=4),
             )
             raise LoginError(contents)
         if "success" in contents and not contents["success"]:
             _LOGGER.error(json.dumps(contents, indent=4))
             raise LoginError(contents)
         if "success" not in contents or "data" not in contents:
-            _LOGGER.error("Unexpected response format: %s",
-                          json.dumps(contents, indent=4))
+            _LOGGER.error(
+                "Unexpected response format: %s", json.dumps(contents, indent=4)
+            )
             raise LoginError(contents)
         data = contents["data"]
         return from_dict(LoginResponse, data["credentials"])
@@ -212,8 +214,7 @@ async def make_api_get_request(
     )
 
     # Create the signing key using the function defined above.
-    signing_key = get_signature_key(
-        credentials.secretKey, date_stamp, REGION, SERVICE)
+    signing_key = get_signature_key(credentials.secretKey, date_stamp, REGION, SERVICE)
     signature = hmac.new(
         signing_key, string_to_sign.encode("utf-8"), hashlib.sha256
     ).hexdigest()
@@ -336,6 +337,5 @@ async def fetch_appliance_infos(credentials: LoginResponse) -> list[ApplianceInf
             credentials,
             canonical_uri=f"/my-homes/{home.id}",
         )
-        appliances.extend(from_dict(HomeResponseData,
-                          home_resp["data"]).appliances)
+        appliances.extend(from_dict(HomeResponseData, home_resp["data"]).appliances)
     return appliances
