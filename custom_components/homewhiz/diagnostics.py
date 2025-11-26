739f5f1d871c50847feaeee74e30cfeecc8eee78
Johnny Willemsen <jwillemsen@remedy.nl>
2025-11-26 16:12:03 +0100

    precommit
        * custom_components/homewhiz/diagnostics.py:

--- custom_components/homewhiz/diagnostics.py
+++ custom_components/homewhiz/diagnostics.py
@@ -1,5 +1,4 @@
 """Diagnostics support for Daikin Diagnostics."""
-
 from __future__ import annotations
 
 from typing import Any
@@ -7,12 +6,10 @@ from typing import Any
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-
-async def async_get_config_entry_diagnostics(
-    hass: HomeAssistant, entry: ConfigEntry
-) -> dict[str, Any]:
+async def async_get_config_entry_diagnostics(hass: HomeAssistant, entry: ConfigEntry) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
     return {
         "data": entry.data["contents"],
-        "appliance_info": entry.data["appliance_info"],
+        "appliance_info": entry.data["appliance_info"]
     }
+
