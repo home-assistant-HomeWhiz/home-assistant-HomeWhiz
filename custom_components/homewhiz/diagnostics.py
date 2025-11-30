9f53a2a1fffdbb394f045dec895af115507a240d
Johnny Willemsen <jwillemsen@remedy.nl>
2025-11-30 13:25:59 +0100

    precommit fixes
        * custom_components/homewhiz/diagnostics.py:

--- custom_components/homewhiz/diagnostics.py
+++ custom_components/homewhiz/diagnostics.py
@@ -1,4 +1,5 @@
 """Diagnostics support for HomeWhiz Diagnostics."""
+
 from __future__ import annotations
 
 from typing import Any
@@ -6,10 +7,12 @@ from typing import Any
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-async def async_get_config_entry_diagnostics(hass: HomeAssistant, entry: ConfigEntry) -> dict[str, Any]:
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
     return {
         "data": entry.data["contents"],
-        "appliance_info": entry.data["appliance_info"]
+        "appliance_info": entry.data["appliance_info"],
     }
-
